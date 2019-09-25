040ab6912e55294d7adb13a6a05e3bfc9f0e8f5f
Jodie Awonmami Azar <jodieazar@Jodies-MacBook-Pro.local>
2019-09-25 11:08:16 -0400

    Initial commit

---
+++ src/KarelFigureEight.java
@@ -0,0 +1,18 @@
+import becker.robots.*;
+
+public class KarelFigureEight {
+
+    public static void main(String[] args) {
+
+        City ssfs = new City();
+
+        Robot karel = new Robot(ssfs,5, 3, Direction.NORTH, 0);
+
+        Thing thing1 = new Thing(ssfs, 2, 3);
+        Thing thing2 = new Thing(ssfs, 4, 3);
+
+
+
+
+    }
+}
