f55932625da5d65c326c4d56eb3c2c9e08747254
jodieazar <Jodieazar@gmail.vom>
2019-10-01 21:14:25 -0400

    RectangleFun

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -1,2 +1,30 @@
+import java.awt.*;
+import java.util.Scanner;
+import java.util.function.DoubleBinaryOperator;
+
 public class RectangleFun {
+    public static void main(String[] args) {
+
+        Scanner rf= new Scanner(System.in);
+        System.out.println("Width: ");
+        int width= rf.nextInt();
+        System.out.println("Height: ");
+        int height= rf.nextInt();
+        System.out.println("X: ");
+        int x= rf.nextInt();
+        System.out.println("Y: ");
+        int y= rf. nextInt();
+
+        Rectangle r= new Rectangle(x,y,width,height);
+        double perimeter= (r.getWidth()+ r.getHeight())*2;
+        System.out.println("Perimeter= " + perimeter);
+        r.setLocation(x-4, y+4);
+        System.out.println("New Location is "+ "("+r.getX()+","+r.getY()+")");
+
+
+        
+
+
+
+    }
 }
